95c4ed4400bfe1cdbed765fc6e30d15dfe59ac4c
Oliver Nightingale <oliver.n@new-bamboo.co.uk>
2011-06-02 23:18:15 +0100

    better implementation of defineMethod

--- src/supplement.js
+++ src/supplement.js
@@ -1,17 +1,18 @@
 supplement = {
-
-  canDefineProperty: (!!Object.defineProperty),
-
   defineMethod: function (obj, name, fn) {
     if (obj[name]) return
 
-    if (this.canDefineProperty) {
-      Object.defineProperty(obj, name, {
-        value: fn,
-        enumerable: false,
-        configurable: false
-      })
-    } else {
+    if (typeof Object.defineProperty == 'function') {
+      try {
+        Object.defineProperty(obj, name, {
+          value: fn,
+          enumerable: false,
+          configurable: false
+        })
+      } catch (e) { /* catch for IE8's broken defineProperty implementation */ }
+    }
+
+    if (!obj[name]) {
       obj[name] = fn
     };
   }
